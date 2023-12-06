8a9d17819c5a9c3cda84212ef5748e3a8eccd85c
aitor-arronte <aitoraalvarez@gmail.com>
2023-12-05 14:00:32 -1000

    added minor

--- model_asr.sh
+++ model_asr.sh
@@ -4,7 +4,8 @@
 #facebook/Wav2Vec2-XLS-R-2B
 #facebook/mms-1b-all
 
-python models/asr.py  --model_id='facebook/wav2vec2-xls-r-300m' \
+python models/asr.py  \
+--model_id='facebook/wav2vec2-xls-r-300m' \
 --lang='ar'\
 --dataset='mozilla-foundation/common_voice_11_0' \
 --output_dir='fine_tuned_models'\
